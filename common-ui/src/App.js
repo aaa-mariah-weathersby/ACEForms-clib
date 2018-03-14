8f933f0e4aaf6ecd4575f47de063f25c49a0993b
TID0027 <tid0027@w1amacmobile006.ace.aaaclubnet.com>
2018-03-14 13:58:01 -0700

    config enzyme

--- common-ui/src/App.js
+++ common-ui/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import input from './input/Input.js'
 
+
 class App extends Component {
   render() {
     return (
--- common-ui/src/input/Input.test.js
+++ common-ui/src/input/Input.test.js
@@ -1,15 +1,29 @@
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Input from './Input';
 
-it('renders an input element', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Input />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Input", () => {
+  let field;
+
+  beforeEach( () => {
+    field = shallow(<Input />);
+  });
+
+  it('stores the current values onSubmit', () => {
+
+  })
 
 });
 
-it('stores the current values onChange', () => {
-  let field = ReactDOM.render(<Input />, div);
-
-})
+// it('renders an input element', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Input />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+//
+// });
+//
+// it('stores the current values onChange', () => {
+//   let field = ReactDOM.render(<Input />, div);
+//
+// })
